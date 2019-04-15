2356ac8d22788e6f1237e850b485ea4554674f4d
Iker Vazquez Lopez <ikervazquezlopez@u.boisestate.edu>
2019-04-15 14:10:22 -0600

    Video browsing and playing with a simple GUI.

--- player.py
+++ player.py
@@ -1,42 +1,54 @@
-from tkinter import *
+import tkinter as tk
+import tkinter.filedialog
 import cv2
-from PIL import Image, ImageTk
 
 video_path = "video.mp4"
 
 canvas_width = 800
 canvas_height =700
 
-master = Tk()
+master = tk.Tk()
 
-canvas = Canvas(master, 
-           width=canvas_width, 
-           height=canvas_height)
-canvas.pack()
+def browse():
+	global video_path 
+	video_path = tk.filedialog.askopenfile().name
 
-#img = PhotoImage(file='testImage.png')
-array = cv2.imread("testImage.png")
-frame = ImageTk.PhotoImage(image=Image.fromarray(array))
-canvas_img = canvas.create_image(20,20, anchor=NW, image=frame)
 
-cap = cv2.VideoCapture(video_path)
 def play():
-	_, frame = cap.read()
-	frame = ImageTk.PhotoImage(image=Image.fromarray(frame))
-	canvas.itemconfig(canvas_img, image = frame)
-	"""
+	print(video_path)
 	cap = cv2.VideoCapture(video_path)
 	frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 	frame_heigth = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 	frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 	for _ in range(0, frame_count):
 		_, frame = cap.read()
-		img = ImageTk.PhotoImage(image=Image.fromarray(frame))
-		canvas.itemconfig(canvas_img, image = img)
-	"""
+		frame = cv2.resize(frame, (int(canvas_width),int(canvas_height)))
+		cv2.imshow(video_path, frame)
+		cv2.waitKey(30)
+	cv2.destroyAllWindows()
+
+browse_button = tk.Button(master, text="Browse", command=browse)
+browse_button.grid(row=0, column=0)
+
+play_button = tk.Button(master, text="Play", command=play)
+play_button.grid(row=1,column=0)
+
+obj_label = tk.Label(master, text="Objects:")
+obj_label.grid(row=0, column=1)
+
+obj_0 = tk.IntVar()
+obj_cb_0 = tk.Checkbutton(master, text="0", variable=obj_0)
+obj_cb_0.grid(row=1, column=1)
+
+obj_1 = tk.IntVar()
+obj_cb_1 = tk.Checkbutton(master, text="1", variable=obj_1)
+obj_cb_1.grid(row=2, column=1)
 
 
-play_button = Button(master, text="Play", command=play)
-play_button.pack()
+obj_2 = tk.IntVar()
+obj_cb_2 = tk.Checkbutton(master, text="2", variable=obj_2)
+obj_cb_2.grid(row=3, column=1)
 
-mainloop()
+
+
+tk.mainloop()
